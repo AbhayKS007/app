f3cabcb7eea40f9506976fc0466f4c280468000e
AbhayKS007 <42775863+AbhayKS007@users.noreply.github.com>
2023-10-16 19:28:09 +0530

    Update guest.sql

--- src/main/resources/databases/guest.sql
+++ src/main/resources/databases/guest.sql
@@ -1,28 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 06, 2017 at 07:55 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `bookstore`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `book`
 --
 
@@ -42,7 +17,3 @@ INSERT INTO `guest` (`des`,`role`) VALUES
 ('abc'),
 ('bcd'),
 ('def');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
